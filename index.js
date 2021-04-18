e2b61d4dab0e0ebb1eecf46673953d40a5a1be43
Piotr Domian <domianpiotr111@gmail.com>
2021-04-18 20:09:50 +0200

    Task is done

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
-function filterItems() {
-  // Zaimplementuj zadanie zgodnie z wytycznymi
+function filterItems(arr1, arr2)
+{
+  var even=[];
+  for(i = 0; i < arr1.length && arr2.length; i++)
+  {
+    if(arr1[i] && arr2[i] > 0)
+    {
+      even.push(arr1[i] + arr2[i])
+    }
+  }
+  return even;
 };
 
 module.exports = filterItems;
+
+
+ 
