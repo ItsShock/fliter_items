8057ed8efba4f45a4068425a48f4d54d2ee80918
Piotr Domian <domianpiotr111@gmail.com>
2021-04-19 09:59:08 +0200

    Task is done

--- index.js
+++ index.js
@@ -1,14 +1,9 @@
-function filterItems(arr1, arr2)
-{
-  var even=[];
-  for(i = 0; i < arr1.length && arr2.length; i++)
-  {
-    if(arr1[i] && arr2[i] > 0)
-    {
-      even.push(arr1[i] + arr2[i])
-    }
-  }
-  return even;
+const filterItems = (arr1, arr2) => {
+  const positive = (n) => n > 0,
+        pos_arr1 = arr1.filter( positive ),
+        pos_arr2 = arr2.filter( positive );
+ 
+  return pos_arr1.concat(pos_arr2);
 };
 
 module.exports = filterItems;
